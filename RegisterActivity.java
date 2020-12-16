e32627304155a0e82e8f08d2dbd58da41c6757e8
rukiyeduru <65501808+rukiyeduru@users.noreply.github.com>
2020-12-16 13:34:29 +0300

    RegisterModulü

--- RegisterActivity.java
+++ RegisterActivity.java
@@ -1,12 +1,20 @@
 package com.example.chatingtime;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
@@ -21,6 +29,8 @@ public class RegisterActivity extends AppCompatActivity {
         actionbarRegister=(Toolbar) findViewById(R.id.actionbarRegister);
         setSupportActionBar(actionbarRegister);
         getSupportActionBar().setTitle("Hesap Oluştur");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         auth=FirebaseAuth.getInstance();/*istediğimiz kontolü oluştuduk*/
 
         txtUsername=(EditText) findViewById(R.id.txtUsernameRegister);
@@ -28,13 +38,56 @@ public class RegisterActivity extends AppCompatActivity {
         txtPassword=(EditText) findViewById(R.id.txtPassowrdRegister);
         btnRegister=(Button) findViewById(R.id.btnRegister);
         btnLoginRegister=(Button) findViewById(R.id.btnLoginRegister);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createNewAccount();
+            }
+        });
+    }
+
+    private void createNewAccount() {/*verileriçekiyoruz*/
+        String username = txtUsername.getText().toString();
+        String email = txtEmail.getText().toString();
+        String password = txtPassword.getText().toString();
+        if(TextUtils.isEmpty(email)){
+
+            Toast.makeText(this,"Email Alanı Boş Olmaz!",Toast.LENGTH_LONG).show();
+
+        }else if(TextUtils.isEmpty(password)){
+            Toast.makeText(this,"Şifre Alanı Boş Olamaz!",Toast.LENGTH_LONG).show();
+
+        }else{
+
+            auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+
+                    if(task.isSuccessful())/*görev başarılı ise yan e mail ve şifre girildiyse*/ {
+
+                        Toast.makeText(RegisterActivity.this,"Hesap Oluşturdunuz !",Toast.LENGTH_LONG).show();
+                        Intent loginIntent = new Intent(RegisterActivity.this,LoginActivity.class);
+                        startActivity(loginIntent);
+                        finish();
+
+                    }else{
+                        Toast.makeText(RegisterActivity.this,"Bir Hata Oluştu !",Toast.LENGTH_LONG).show();
+                    }
+
+                }
+            });
+
+        }
+
     }
 
 
-        @Override
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        init();
 
     }
 }
