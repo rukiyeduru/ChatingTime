63808ba8283cbe1331c0bea0645a7c335d4c0a9f
rukiyeduru <65501808+rukiyeduru@users.noreply.github.com>
2021-03-30 17:13:18 +0300

    Add files via upload

---
+++ ayarlarActivity.java
@@ -0,0 +1,32 @@
+package com.example.chatapp1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class SettingActivity extends AppCompatActivity {
+
+    private Button UpdateAccountSettings;
+    private EditText userName,userStatus;
+    private CircleImageView userProfileImage;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+        init();
+    }
+
+    private void init() {
+        UpdateAccountSettings=(Button) findViewById(R.id.update_setting_button);
+        userName=(EditText) findViewById(R.id.set_user_name);
+        userStatus=(EditText) findViewById(R.id.set_profile_status);
+        userProfileImage=(CircleImageView) findViewById(R.id.set_profile_image);
+    }
+}
